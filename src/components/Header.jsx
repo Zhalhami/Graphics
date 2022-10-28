3bc0a880ab99f495a16282e614fbc1968276bef1
Zhalhami <73404711+Zhalhami@users.noreply.github.com>
2022-10-28 20:16:09 +0100

    Completed react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header(){
         <div className="container-fluid">
         <nav className="navbar navbar-expand-lg fixed-top  navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
+    <NavLink className="navbar-brand" to ="/">
         <img src="images/gaucho.jpeg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top">
-    </img>GAUCHO DESIGN</a>
+    </img>GAUCHO DESIGN</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Subtopic from "./Subtopic";
+
 
 function Portfolio (){
 return(
