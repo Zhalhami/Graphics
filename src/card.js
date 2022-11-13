d211d221cff23b71dc3a2221f6714f3db31fc42a
Zhalhami <73404711+Zhalhami@users.noreply.github.com>
2022-11-13 10:45:34 +0100

    modify card content

--- src/card.js
+++ src/card.js
@@ -2,37 +2,37 @@ const cards =[
     {
         imgUrl:"https://cdn-icons-png.flaticon.com/512/1540/1540247.png" ,
         title:"Manipulation" ,
-        text :"Best in manipulation of any of your graphical content.",
+        text :"Manipulation of any graphical content.",
         id:1
     },
     {
         imgUrl:"https://cdn-icons-png.flaticon.com/512/29/29914.png" ,
         title:"Flyer" ,
-        text :"Best in making flyer for your business advertisment. " ,
+        text :"Flyer for business advertisment. " ,
         id:2
     },
     {
         imgUrl:"https://cdn-icons-png.flaticon.com/512/1832/1832062.png" ,
         title:"Cartoon Work" ,
-        text :"Tranforming your pictures into cartoon." ,
+        text :"Tranforming your pictures into cartoons." ,
         id:3
     },
     {
         imgUrl:"https://cdn-icons-png.flaticon.com/512/741/741942.png" ,
         title:"ID Cards" ,
-        text :"Best in making a valid usable ID card for schools or works." ,
+        text :"Valid usable ID card for schools or works." ,
         id:4
     },
     {
         imgUrl:"https://cdn-icons-png.flaticon.com/512/4596/4596883.png" ,
         title:"Logo Design",
-        text :"Branding your bussiness with the very best logo design." ,
+        text :"Branding your business with the very best logo." ,
         id:5
     },
     {
         imgUrl:"https://cdn-icons-png.flaticon.com/512/5171/5171767.png" ,
         title:"Banner",
-        text :"Making banners for all activites that requires banners. " ,
+        text :"Making attractive banners for all activites. " ,
         id:6
     }
 ]
